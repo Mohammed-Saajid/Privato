fdea1979afa8122315e9ff3f5d6f3d400b7ef667
Mohammed-Saajid <alaudinsmile4444@gmail.com>
2025-09-21 22:00:39 +0530

    Added Examples

--- examples/cli-analyze.bat
+++ examples/cli-analyze.bat
@@ -1,4 +1,4 @@
-python -m cli.main analyzer analyze examples/data/pdfs --output-path examples/data/pdfs_out --hide-output  --save-output
-python -m cli.main analyzer analyze examples/data/images --output-path examples/data/images_out --hide-output --save-output
-python -m cli.main analyzer analyze examples/data/pdfs/Emily_Dawson.pdf --output-path examples/data/pdfs_out --hide-output --save-output
-python -m cli.main analyzer analyze examples/data/images/X_000.jpeg --output-path examples/data/images_out --hide-output --save-output
+python -m privato.cli.main analyzer analyze examples/data/pdfs --output-path examples/data/pdfs_out --hide-output  --save-output
+python -m privato.cli.main analyzer analyze examples/data/images --output-path examples/data/images_out --hide-output --save-output
+python -m privato.cli.main analyzer analyze examples/data/pdfs/Emily_Dawson.pdf --output-path examples/data/pdfs_out --hide-output --save-output
+python -m privato.cli.main analyzer analyze examples/data/images/X_000.jpeg --output-path examples/data/images_out --hide-output --save-output
--- examples/cli-redact.bat
+++ examples/cli-redact.bat
@@ -1,4 +1,4 @@
-python -m cli.main redactor redact examples/data/pdfs examples/data/pdfs_out
-python -m cli.main redactor redact examples/data/images examples/data/images_out
-python -m cli.main redactor redact examples/data/pdfs/Emily_Dawson.pdf examples/data/pdfs_out
-python -m cli.main redactor redact examples/data/images/X_000.jpeg examples/data/images_out
+python -m privato.cli.main redactor redact examples/data/pdfs examples/data/pdfs_out
+python -m privato.cli.main redactor redact examples/data/images examples/data/images_out
+python -m privato.cli.main redactor redact examples/data/pdfs/Emily_Dawson.pdf examples/data/pdfs_out
+python -m privato.cli.main redactor redact examples/data/images/X_000.jpeg examples/data/images_out
